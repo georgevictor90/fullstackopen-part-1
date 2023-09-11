f3d8497b6237fbb0a0350a988e67a3fa75e62e02
Victor Lacatus <georgevictorl@yahoo.com>
2023-09-12 00:42:31 +0300

    add anecdotes solution

---
+++ anecdotes/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Button from "./components/Button";
+import Anecdote from "./components/Anecdote";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+
+  const showRandomAnecdote = () => {
+    const randomAnecdoteIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomAnecdoteIndex);
+  };
+
+  const vote = () => {
+    const copy = [...points];
+    copy[selected] += 1;
+    setPoints(copy);
+  };
+
+  const getMostVotedAnecdote = () => {
+    const maxPoints = Math.max(...points);
+    const index = points.indexOf(maxPoints);
+    const mostVotedAnecdote = anecdotes[index];
+    return mostVotedAnecdote;
+  };
+
+  return (
+    <>
+      <h2>Anecdote of the day</h2>
+      <Anecdote anecdote={anecdotes[selected]} />
+      <p>Has {points[selected]} votes.</p>
+      <Button handler={vote} text={"vote"} />
+      <Button handler={showRandomAnecdote} text={"next anecdote"} />
+
+      <h2>Anecdote with most votes</h2>
+      <Anecdote anecdote={getMostVotedAnecdote()} />
+    </>
+  );
+};
+
+export default App;
---
+++ anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Anecdote = ({ anecdote }) => {
+  return <div>{anecdote}</div>;
+};
+
+export default Anecdote;
