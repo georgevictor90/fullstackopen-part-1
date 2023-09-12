b5fa7520b64dcf12e6e151ca44414161e962f8e9
Victor Lacatus <georgevictorl@yahoo.com>
2023-09-12 20:53:53 +0300

    show statistics in table

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -23,14 +23,16 @@ const App = () => {
 
       <Statistics>
         {all ? (
-          <>
-            <StatisticsLine text={"good"} value={good} />
-            <StatisticsLine text={"neutral"} value={neutral} />
-            <StatisticsLine text={"bad"} value={bad} />
-            <StatisticsLine text={"all"} value={all} />
-            <StatisticsLine text={"average"} value={average} />
-            <StatisticsLine text={"positive"} value={positive + " %"} />
-          </>
+          <table>
+            <tbody>
+              <StatisticsLine text={"good"} value={good} />
+              <StatisticsLine text={"neutral"} value={neutral} />
+              <StatisticsLine text={"bad"} value={bad} />
+              <StatisticsLine text={"all"} value={all} />
+              <StatisticsLine text={"average"} value={average} />
+              <StatisticsLine text={"positive"} value={positive + " %"} />
+            </tbody>
+          </table>
         ) : (
           <p>No feedback given</p>
         )}
